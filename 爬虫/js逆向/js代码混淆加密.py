31c14ac8626f4d7b7f6285230e847424e96048c4
kidword <35066211+kidword@users.noreply.github.com>
2020-07-02 14:41:47 +0800

    Update js代码混淆加密.py

--- 爬虫/js逆向/js代码混淆加密.py
+++ 爬虫/js逆向/js代码混淆加密.py
@@ -1,6 +1,3 @@
-"""
-js 加密混淆
-"""
 import hashlib
 import time
 import base64
@@ -8,19 +5,13 @@ from typing import List, Any
 import requests
 
 INDEX_URL = 'https://dynamic6.scrape.cuiqingcai.com/api/movie?limit={limit}&offset={offset}&token={token}'
+DETAIL_URL = 'https://dynamic6.scrape.cuiqingcai.com/api/movie/{id}?token={token}'
 LIMIT = 10
 OFFSET = 0
+SECRET = 'ef34#teuq0btua#(-57w1q5o5--j@98xygimlyfxs*-!i-0-mb'
 
 
 def get_token(args: List[Any]):
-    """
-    先将/api/movie放到列表里面；
-    列表中加入时间戳
-    将列表内容用逗号拼接
-    将拼接结果进行SHA1编码
-    将编码结果和时间戳再次拼接
-    将拼接后结果进行base64编码
-    """
     timestamp = str(int(time.time()))
     args.append(timestamp)
     sign = hashlib.sha1(','.join(args).encode('utf-8')).hexdigest()
@@ -31,4 +22,13 @@ args = ['/api/movie']
 token = get_token(args=args)
 index_url = INDEX_URL.format(limit=LIMIT, offset=OFFSET, token=token)
 response = requests.get(index_url)
-print(response.text)
+
+result = response.json()
+for item in result['results']:
+    id = item['id']
+    encrypt_id = base64.b64encode((SECRET + str(id)).encode('utf-8')).decode('utf-8')
+    args = [f'/api/movie/{encrypt_id}']
+    token = get_token(args=args)
+    detail_url = DETAIL_URL.format(id=encrypt_id, token=token)
+    response = requests.get(detail_url)
+    print('response', response.json())
