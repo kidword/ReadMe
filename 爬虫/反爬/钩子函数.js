d80c08c475fb9d77ea24a525408fb7b794821735
kidword <35066211+kidword@users.noreply.github.com>
2020-07-06 11:31:26 +0800

    Update 钩子函数.js

--- 爬虫/反爬/钩子函数.js
+++ 爬虫/反爬/钩子函数.js
@@ -1,6 +1,6 @@
-# 参考地址：https://zhuanlan.zhihu.com/p/108207751?from_voters_page=true
+// 参考地址：https://zhuanlan.zhihu.com/p/108207751?from_voters_page=true
 
-1.用于定位cookie中关键参数生成位置
+//1.用于定位cookie中关键参数生成位置
 
 var code = function(){
     var org = document.cookie.__lookupSetter__('cookie');
@@ -16,9 +16,9 @@ var script = document.createElement('script');
 script.textContent = '(' + code + ')()';
 (document.head||document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
-当cookie中匹配到了 TSdc75a61a， 则插入断点。
+//当cookie中匹配到了 TSdc75a61a， 则插入断点。
 
-2 请求钩子
+//2 请求钩子
 
 var code = function(){
 var open = window.XMLHttpRequest.prototype.open;
@@ -35,7 +35,7 @@ script.textContent = '(' + code + ')()';
 script.parentNode.removeChild(script);
 当请求的url里包含MmEwMD时，则插入断点
 
-3 header钩子
+//3 header钩子
 var code = function(){
 var org = window.XMLHttpRequest.prototype.setRequestHeader;
 window.XMLHttpRequest.prototype.setRequestHeader = function(key,value){
@@ -50,4 +50,4 @@ script.textContent = '(' + code + ')()';
 (document.head||document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
 
-当header中包含Authorization时，则插入断点
+//当header中包含Authorization时，则插入断点
